8486a36f315d850ce4a52d65d3dffa29f931d5e8
Anggapur <arimbawaangga@gmail.com>
2018-09-08 22:53:14 +0800

    TTD sip 2

--- app/TTD.php
+++ app/TTD.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class TTD extends Model
 {
     //
-    protected $table = "TTD";
+    protected $table = "ttd";
     protected $fillable = ["halaman","bagian","nilai1","nilai2","nilai3","nilai4","nilai5","kd_satker","image"];
 }
